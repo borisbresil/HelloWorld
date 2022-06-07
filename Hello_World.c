165629f83f35543d1dc375b0d028533fa817872c
borisbresil <boris.b.bresil@gmail.com>
2022-06-07 18:10:38 +0200

    Sentence corrected

--- Hello_World.c
+++ Hello_World.c
@@ -5,6 +5,7 @@ int main(void)
 {
   printf("Bonjour le monde !\n");
   printf("Comment ça va ?\n");
+  printf("Je vais bien merci! \n");
   printf("Je teste le retour vers le futur :)\n");
   return 0;
 }
