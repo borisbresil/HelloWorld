16e396a7f51a15d9c6aea3dcc1bcfe384d41f1b1
borisbresil <boris.b.bresil@gmail.com>
2022-06-07 23:00:09 +0200

    Ask questions to user

--- Hello_World.c
+++ Hello_World.c
@@ -4,9 +4,15 @@
 int main(void)
 {
   int age;
+  int user_age;
+  
   age = 34;
+  user_age = 0;
+    
   printf("Bonjour le monde !\n");
-  printf("Comment ça va ?\n");
+  printf("Quel âge avez-vous ?\n");
+  scanf("%d",&user_age);
+  printf("Vous avez donc %d ans !\n\n", user_age);
   printf("J'ai %d ans\n", age);
   printf("Je vais bien merci! \n");
   printf("Je teste le retour vers le futur :)\n");
