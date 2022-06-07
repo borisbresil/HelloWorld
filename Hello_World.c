37412f7a74df8901c5f4cf2603375d9098308276
borisbresil <boris.b.bresil@gmail.com>
2022-06-07 22:25:19 +0200

    j'ai %d ans -added

--- Hello_World.c
+++ Hello_World.c
@@ -3,8 +3,11 @@
 
 int main(void)
 {
+  int age;
+  age = 34;
   printf("Bonjour le monde !\n");
   printf("Comment ça va ?\n");
+  printf("J'ai %d ans\n", age);
   printf("Je vais bien merci! \n");
   printf("Je teste le retour vers le futur :)\n");
   return 0;
