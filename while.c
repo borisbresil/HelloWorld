5661abf55693f9c858daa6df454fb6f543e2b515
borisbresil <boris.b.bresil@gmail.com>
2022-06-12 14:25:25 +0200

    added a count limit

--- while.c
+++ while.c
@@ -5,12 +5,15 @@ int main(void)
 {
 
 int num_input;
+int count;
 
 num_input = 0;
+count = 0;
 
-while (num_input != 42)
+while (num_input != 42 && count <= 5)
 {
 	printf("Please enter a number: \n");
 	scanf("%d", &num_input);
+	count++;
 }
 }
